5c5745a82cbac79549b387f50a69a836ac396019
aracope <156398622+aracope@users.noreply.github.com>
2025-01-18 12:51:26 -0800

    Add HTML file; change variables to arrow functions.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-function squareAndFindEvens(numbers){
-    var squares = numbers.map(function(num){
-      return num ** 2;
-    });
-    var evens = squares.filter(function(square){
-      return square % 2 === 0;
-    });
-    return evens;
-  }
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+  function squareAndFindEvens(numbers){
+    var squares = numbers.map(num =>  num ** 2);
+    var evens = squares.filter(square => square % 2 === 0);
+    }
