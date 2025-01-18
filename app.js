578dc670fb077b74568f3fcbb2e1a4947c6d2ff5
aracope <156398622+aracope@users.noreply.github.com>
2025-01-18 12:29:37 -0800

    Initial commit of exercise function.

---
+++ app.js
@@ -0,0 +1,9 @@
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
